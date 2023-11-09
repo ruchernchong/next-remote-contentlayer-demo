faf259318ab03613d7c88718044427e08e635426
Ru Chern Chong <hello@ruchern.xyz>
2023-11-09 23:19:43 +0800

    Add SST

---
+++ sst.config.ts
@@ -0,0 +1,25 @@
+import { SSTConfig } from "sst";
+import { NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "next-contentlayer-demo",
+      region: "ap-southeast-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, "site", {
+        customDomain: {
+          domainName: "next-remote-contentlayer-demo.ruchern.xyz",
+          hostedZone: "ruchern.xyz",
+        },
+      });
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
